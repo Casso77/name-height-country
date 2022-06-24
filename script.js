a560b0d33a1f24ab1bddf82a2d99837ac8253796
Casso77 <chimgozirimejim@gmail.com>
2022-06-24 23:57:43 +0100

    html and js
    Added html and javascript code

---
+++ script.js
@@ -0,0 +1,14 @@
+//print out name, height and country on the screen
+
+
+myFunction = function() {
+    var first = document.getElementById("fullname").value;
+    var second = document.getElementById("height").value;
+    var third = document.getElementById("country").value;
+
+    document.getElementById("f1").innerHTML = first;
+    document.getElementById("f2").innerHTML = second;
+    document.getElementById("f3").innerHTML = third;
+
+    // document.getElementById("").innerHTML = first, second, third
+}
